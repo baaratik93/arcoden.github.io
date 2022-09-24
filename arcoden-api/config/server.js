8ded833ee39b9c66c81f55ba1f726029d79756d2
Mahmoud <dooro.mah@gmail.com>
2022-09-24 18:12:47 +0000

    Back et frond du blog

---
+++ arcoden-api/config/server.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
+
+module.exports = ({ env }) => ({
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
+});
+ 
---
+++ arcoden-next/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    loader: "default",
+    domains: ["localhost"],
+  },
+  
+};
