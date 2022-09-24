bcaab0c6a4f639a65e5f8c3e046b40bec0ab1dbb
Mahmoud <dooro.mah@gmail.com>
2022-09-24 20:33:01 +0000

    Adding logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return <>
   <Spinner/>
   <h1>Ce blog est en cours de construction!!!</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam vero, sapiente laboriosam aut hic error fugiat doloribus nobis consequuntur enim aperiam quasi atque quis illum deserunt inventore porro adipisci architecto, totam necessitatibus quia aspernatur eveniet eaque soluta! Blanditiis voluptatibus ad, mollitia nihil temporibus vitae minus, molestiae enim iure aliquam facilis commodi, deleniti veniam voluptas itaque dignissimos. Ducimus delectus, saepe voluptatem voluptate labore quis possimus rerum et est, unde perferendis natus atque? Nesciunt quidem, rerum soluta fugit illo amet sed hic doloremque accusantium cumque dicta magnam repellendus obcaecati laboriosam? Tenetur dicta libero deserunt repellat modi? Suscipit porro quae commodi ad amet.
+  </p>
   </>
 }
 
