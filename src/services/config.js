81359f98d244ee89fbde28589cc6200d3c0010e2
Mamadou THIAM <dooro.mah@gmail.com>
2022-10-12 12:00:26 +0200

    Add firebase to the project

---
+++ src/services/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB6ZCKMUtOv1sZuMmxMlp3Ad8_mWAMTfoY",
+  authDomain: "arcoden-f732e.firebaseapp.com",
+  projectId: "arcoden-f732e",
+  storageBucket: "arcoden-f732e.appspot.com",
+  messagingSenderId: "359593111186",
+  appId: "1:359593111186:web:11cda189acb3421c91404b",
+  measurementId: "G-YPRPJ9LMQH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
